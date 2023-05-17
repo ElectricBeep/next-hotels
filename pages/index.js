70f6061c3f9ed0bb0a57794be2a1ef7cdb91f73a
ElectricBeep <adnan.smajic4431@gmail.com>
2023-05-17 14:04:17 +0200

    fixed structured data

--- pages/index.js
+++ pages/index.js
@@ -32,63 +32,64 @@ export default function Home() {
         <link rel="icon" href="/hotel.png" />
 
         <Script
+          key="structured-data"
           type="application/ld+json"
           dangerouslySetInnerHTML={{
             __html: JSON.stringify({
               "@context": "https://schema.org",
               "@type": "ItemList",
-              "itemListElement": [
+              itemListElement: [
                 {
                   "@type": "ListItem",
-                  "position": 1,
-                  "item": {
+                  position: 1,
+                  item: {
                     "@type": "Hotel",
-                    "name": "Hotel Cosmopolit",
-                    "description": "The hotel is situated in the heart of the city, at 15 Radićeva street. It offers a perfect blend of pristine comfort and quality. With 16 luxurious rooms, designed and equipped with the latest world standards, guests can be assured of an exceptional experience. Our hospitable staff will ensure that your stay at our hotel is even more enjoyable.",
-                    "image": "/img/featured/featured7.jpg",
-                    "url": "https://www.hecco-hotels-sarajevo.com/hotel/hotel-cosmopolit",
-                    "address": {
+                    name: "Hotel Cosmopolit",
+                    description: "The hotel is situated in the heart of the city, at 15 Radićeva street. It offers a perfect blend of pristine comfort and quality. With 16 luxurious rooms, designed and equipped with the latest world standards, guests can be assured of an exceptional experience. Our hospitable staff will ensure that your stay at our hotel is even more enjoyable.",
+                    image: "/img/featured/featured7.jpg",
+                    url: "https://www.hecco-hotels-sarajevo.com/hotel/hotel-cosmopolit",
+                    address: {
                       "@type": "PostalAddress",
-                      "streetAddress": "Radićeva 15 Street",
-                      "addressLocality": "Sarajevo",
-                      "postalCode": "71000",
-                      "addressCountry": "Bosnia and Herzegovina"
+                      streetAddress: "Radićeva 15 Street",
+                      addressLocality: "Sarajevo",
+                      postalCode: "71000",
+                      addressCountry: "Bosnia and Herzegovina"
                     }
                   }
                 },
                 {
                   "@type": "ListItem",
-                  "position": 2,
-                  "item": {
+                  position: 2,
+                  item: {
                     "@type": "Hotel",
-                    "name": "Hotel City View Deluxe",
-                    "description": "The City View Deluxe Hotel is situated in the heart of Sarajevo, on Ferhadija Street, occupying the top four floors of a business building. From here, guests can enjoy breathtaking views of the entire city.",
-                    "image": "/img/featured/featured3.jpg",
-                    "url": "https://www.hecco-hotels-sarajevo.com/hotel/hotel-hecco-deluxe",
-                    "address": {
+                    name: "Hotel City View Deluxe",
+                    description: "The City View Deluxe Hotel is situated in the heart of Sarajevo, on Ferhadija Street, occupying the top four floors of a business building. From here, guests can enjoy breathtaking views of the entire city.",
+                    image: "/img/featured/featured3.jpg",
+                    url: "https://www.hecco-hotels-sarajevo.com/hotel/hotel-hecco-deluxe",
+                    address: {
                       "@type": "PostalAddress",
-                      "streetAddress": "Ferhadija 2 Street",
-                      "addressLocality": "Sarajevo",
-                      "postalCode": "71000",
-                      "addressCountry": "Bosnia and Herzegovina"
+                      streetAddress: "Ferhadija 2 Street",
+                      addressLocality: "Sarajevo",
+                      postalCode: "71000",
+                      addressCountry: "Bosnia and Herzegovina"
                     }
                   }
                 },
                 {
                   "@type": "ListItem",
-                  "position": 3,
-                  "item": {
+                  position: 3,
+                  item: {
                     "@type": "Hotel",
-                    "name": "Hotel Hecco",
-                    "description": "Set in a modern building and renovated in 2018, just a 5-minute walk from Sarajevo’s cultural centre, Hotel Hecco provides air-conditioned rooms with free Wi-Fi. It offers a restaurant and organised tours. This hotel uses renewable energy sources only.",
-                    "image": "/img/featured/featured6.jpg",
-                    "url": "https://www.hecco-hotels-sarajevo.com/hotel/hotel-hecco",
-                    "address": {
+                    name: "Hotel Hecco",
+                    description: "Set in a modern building and renovated in 2018, just a 5-minute walk from Sarajevo’s cultural centre, Hotel Hecco provides air-conditioned rooms with free Wi-Fi. It offers a restaurant and organised tours. This hotel uses renewable energy sources only.",
+                    image: "/img/featured/featured6.jpg",
+                    url: "https://www.hecco-hotels-sarajevo.com/hotel/hotel-hecco",
+                    address: {
                       "@type": "PostalAddress",
-                      "streetAddress": "Medresa 1 Street",
-                      "addressLocality": "Sarajevo",
-                      "postalCode": "71000",
-                      "addressCountry": "Bosnia and Herzegovina"
+                      streetAddress: "Medresa 1 Street",
+                      addressLocality: "Sarajevo",
+                      postalCode: "71000",
+                      addressCountry: "Bosnia and Herzegovina"
                     }
                   }
                 }
