6a5735779138e2535fd23bdbbb4867c25043be23
ElectricBeep <adnan.smajic4431@gmail.com>
2023-05-24 13:20:45 +0200

    format

--- pages/hotel/hotel-cosmopolit.jsx
+++ pages/hotel/hotel-cosmopolit.jsx
@@ -185,7 +185,7 @@ const HotelCosmopolit = () => {
             >
               <motion.img
                 variants={slideIn("right", "spring", 0.4, 1)}
-                src="/img/featured/featured7.jpg"
+                src="/img/featured/featured7.webp"
                 alt="hotel cosmopolit"
                 className={styles.rightImg}
               />
