f9820effa08752790fecf229bd090686b8ca9373
ElectricBeep <adnan.smajic4431@gmail.com>
2023-05-24 11:08:41 +0200

    updated framer motion version

--- components/PhotoSlider.jsx
+++ components/PhotoSlider.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Link from "next/link";
 import { MdChevronLeft, MdChevronRight } from "react-icons/md";
-import { BsArrowRightShort } from "react-icons/bs";
 import { motion } from "framer-motion";
 
 import styles from "../styles/PhotoSlider.module.css";
